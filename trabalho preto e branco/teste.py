ec3887de8b08399323c5ef8c9e5226c2ac1daa60
HugoVec <42803993+HugoVec@users.noreply.github.com>
2021-10-19 21:15:40 -0300

    Segundo commit

--- trabalho preto e branco/teste.py
+++ trabalho preto e branco/teste.py
@@ -1,86 +1,57 @@
+
+
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('pirata.png',0)
+import tkinter as tk
+from tkinter import *
+
+from tkinter import filedialog
+from tkinter.filedialog import askopenfile
+from PIL import Image, ImageTk
+
+
+def select_img():
+    global img1
+
+    # Pega o caminho da imagem
+    local = filedialog.askopenfilename()
+
+    # Certifica que tem um caminho
+    if len(local) > 0:
+        return local
+
+
+btnSelectImg = Button(text="Selecione a Imagem", command=select_img)
+btnSelectImg.pack(side="bottom", fill="both", expand="yes", padx="10", pady="10")
+
+img = cv2.imread(select_img(), 0)
 
 # global thresholding
-ret1,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+ret1, th1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
 
 # Otsu's thresholding
-ret2,th2 = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+ret2, th2 = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
 # Otsu's thresholding after Gaussian filtering
-blur = cv2.GaussianBlur(img,(5,5),0)
-ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+blur = cv2.GaussianBlur(img, (5, 5), 0)
+ret3, th3 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
 # plot all the images and their histograms
 images = [img, 0, th1,
           img, 0, th2,
           blur, 0, th3]
-titles = ['Original Noisy Image','Histogram','Global Thresholding (v=127)',
-          'Original Noisy Image','Histogram',"Otsu's Thresholding",
-          'Gaussian filtered Image','Histogram',"Otsu's Thresholding"]
+titles = ['Original Noisy Image', 'Histogram', 'Global Thresholding (v=127)',
+          'Original Noisy Image', 'Histogram', "Otsu's Thresholding",
+          'Gaussian filtered Image', 'Histogram', "Otsu's Thresholding"]
 
 for i in range(3):
-    plt.subplot(3,3,i*3+1),plt.imshow(images[i*3],'gray')
-    plt.title(titles[i*3]), plt.xticks([]), plt.yticks([])
-    plt.subplot(3,3,i*3+2),plt.hist(images[i*3].ravel(),256)
-    plt.title(titles[i*3+1]), plt.xticks([]), plt.yticks([])
-    plt.subplot(3,3,i*3+3),plt.imshow(images[i*3+2],'gray')
-    plt.title(titles[i*3+2]), plt.xticks([]), plt.yticks([])
+    plt.subplot(3, 3, i * 3 + 1), plt.imshow(images[i * 3], 'gray')
+    plt.title(titles[i * 3]), plt.xticks([]), plt.yticks([])
+    plt.subplot(3, 3, i * 3 + 2), plt.hist(images[i * 3].ravel(), 256)
+    plt.title(titles[i * 3 + 1]), plt.xticks([]), plt.yticks([])
+    plt.subplot(3, 3, i * 3 + 3), plt.imshow(images[i * 3 + 2], 'gray')
+    plt.title(titles[i * 3 + 2]), plt.xticks([]), plt.yticks([])
 plt.show()
 
-
-
-
-
-#
-#
-# import cv2
-# import numpy as np
-# from matplotlib import pyplot as plt
-#
-# img = cv2.imread('download.jpg',0)
-# ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
-# ret,thresh2 = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)
-# ret,thresh3 = cv2.threshold(img,127,255,cv2.THRESH_TRUNC)
-# ret,thresh4 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO)
-# ret,thresh5 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO_INV)
-#
-# titles = ['Original Image','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']
-# images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
-#
-# for i in range(6):
-#     plt.subplot(2,3,i+1),plt.imshow(images[i],'gray')
-#     plt.title(titles[i])
-#     plt.xticks([]),plt.yticks([])
-#
-# plt.show()
-
-
-
-
-#
-# import cv2
-# import numpy as np
-# from matplotlib import pyplot as plt
-#
-# img = cv2.imread('download.jpg',0)
-# img = cv2.medianBlur(img,5)
-#
-# ret,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
-# th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
-#             cv2.THRESH_BINARY,11,2)
-# th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-#             cv2.THRESH_BINARY,11,2)
-#
-# titles = ['Original Image', 'Global Thresholding (v = 127)',
-#             'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
-# images = [img, th1, th2, th3]
-#
-# for i in range(4):
-#     plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
-#     plt.title(titles[i])
-#     plt.xticks([]),plt.yticks([])
-# plt.show()
